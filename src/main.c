0d1020965047d7a0bc2aad9fdbc28ae28fb0ae6d
Maksim Baranouski <130934043+MaximBaranovskiy@users.noreply.github.com>
2024-03-09 10:24:55 +0000

    Write main.c,countLetter.h,countLetter.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
-/* hello.c */
 #include <stdio.h>
+#include "countLetter.h"
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Использование: %s <предложение> <буква>\n", argv[0]);
+        return 1;
+    }
+
+    char *sentence = argv[1];
+    char letter = argv[2][0];
+
+    int result = countLetter(sentence, letter);
+
+    printf("Количество вхождений буквы %c в предложении: %d\n", letter, result);
+
+    return 0;
 }
